b4cf3c4925e19e3b0cf428824c7b36dacff7c531
mkcnelis <maikel.nelis@gmail.com>
2020-03-20 08:49:16 +0100

    oke?

---
+++ Recept agenda.php
@@ -0,0 +1,16 @@
+
+
+<?php 
+echo date('Y-m-d', strtotime(' + 7 days'));
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AGENDA</title>
+</head>
+<body>
+    
+</body>
+</html>
